0390fadf74c437ea94192bd6c10431b29bfc2cd5
Miguele73 <cabusam.eu@gmail.com>
2025-03-04 15:20:25 +0100

    Zweite Version, finde den Fehler nicht

--- UE 18 Filmabend/Niederschlag.dart
+++ UE 18 Filmabend/Niederschlag.dart
@@ -1,8 +1,8 @@
 void main() {
   List<Map<String, double?>> weatherData = [
-    {'temp': 5.3, 'rain': 0.9, 'wind': 0.0},
-    {'temp': 4.5, 'rain': 0.0, 'wind': 16.8},
-    {'temp': 0.0, 'rain': 3.8, 'wind': 0.0},
+    {'temp': 5.3, 'rain': 0.9, 'wind': null},
+    {'temp': 4.5, 'rain': null, 'wind': 16.8},
+    {'temp': null, 'rain': 3.8, 'wind': null},
   ];
 
   List<double?> temps = [];
@@ -22,17 +22,27 @@ void main() {
   winds.add(weatherData[1]['wind']);
   winds.add(weatherData[2]['wind']);
 
-  double? avgTemp = (temps[0]! + temps[1]! + temps[2]!) / 3;
-  double? avgRain = (rains[0]! + rains[1]! + rains[2]!) / 3;
-  double? avgWind = (winds[0]! + winds[1]! + winds[2]!) / 3;
+  if (temps.contains(null)) {
+    temps.add(0.0);
+  }
+  if (rains.contains(null)) {
+    rains.add(0.0);
+  }
+  if (winds.contains(null)) {
+    winds.add(0.0);
+  }
+
+  double? avgTemp = (temps[0] ?? temps[1] ?? temps[2])! / 3;
+  double? avgRain = (rains[0] ?? rains[1] ?? rains[2])! / 3;
+  double? avgWind = (winds[0] ?? winds[1] ?? winds[2])! / 3;
   print(
     "Durchschnittstemperatur: $avgTemp"
             "°C" +
-        " "
-            "Durchschnitts Niederschlag: $avgRain"
+        " " +
+        "Durchschnitts Niederschlag: $avgRain"
             "mm" +
-        " "
-            "Durchschnittliche Windgeschwindigkeit: $avgWind"
+        " " +
+        "Durchschnittliche Windgeschwindigkeit: $avgWind"
             "km/h",
   );
 }
